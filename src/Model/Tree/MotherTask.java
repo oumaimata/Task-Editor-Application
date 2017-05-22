8d7a12577ef115787420fb37142c2af97f9ce889
Camille Soetaert <camille.soetaert@etu.utc.fr>
2017-05-22 15:53:45 +0200

    Modification des classes existante du modèleet création de nouvelles classes en fonction du schéma UML réalisé après la réunion

---
+++ src/Model/Tree/MotherTask.java
@@ -0,0 +1,10 @@
+package Model.Tree;
+
+import Model.GlobalParameters;
+
+/**
+ * Created by Camille on 22/05/17.
+ */
+public class MotherTask extends Task{
+    private GlobalParameters.TypeConstructeur constructor;
+}
--- src/Model/Tree/Tag.java
+++ src/Model/Tree/Tag.java
@@ -1,8 +1,14 @@
 package Model.Tree;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * Created by Théo on 17/05/2017.
  */
 public class Tag {
+    private IntegerProperty idProperty;
+    private StringProperty nameProperty;
+
 
 }

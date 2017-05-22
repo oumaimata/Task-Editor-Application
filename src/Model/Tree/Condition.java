6b6540cfd5e89c8547353e6642d814f68e5fdbde
Camille Soetaert <camille.soetaert@etu.utc.fr>
2017-05-22 15:53:45 +0200

    Modification des classes existante du modèleet création de nouvelles classes en fonction du schéma UML réalisé après la réunion

--- src/Model/Tree/Condition.java
+++ src/Model/Tree/Condition.java
@@ -3,31 +3,25 @@ package Model.Tree;
 import Model.GlobalParameters;
 import javafx.beans.property.StringProperty;
 
+import java.util.List;
+
 /**
  * Created by Théo on 17/05/2017.
  */
 public class Condition {
-    GlobalParameters.TypeCondition _type;
+    private GlobalParameters.TypeCondition type;
 
-    public GlobalParameters.TypeCondition getType() { return _type; }
-    public void setType(GlobalParameters.TypeCondition type) { this._type = type; }
+    public GlobalParameters.TypeCondition getType() { return type; }
+    public void setType(GlobalParameters.TypeCondition type) { this.type = type; }
 
     public class Assertion {
-        StringProperty _subject;
-        StringProperty _predicate;
-        StringProperty _object;
-        GlobalParameters.TypeAssertion _type;
+        public StringProperty subject;
+        public StringProperty predicate;
+        public StringProperty object;
+        public GlobalParameters.TypeAssertion type;
 
-        /*public String getSubject() { return _subject.getValue(); }
-        public void setSubject(String subject) { this._subject = subject; }
 
-        public String getPredicate() { return _predicate; }
-        public void setPredicate(String predicate) { this._predicate = predicate; }
-
-        public String getObject() { return _object; }
-        public void setObject(String object) { this._object = object; }
-
-        public GlobalParameters.TypeAssertion getType() { return _type; }
-        public void setType(GlobalParameters.TypeAssertion type) { this._type = type; }*/
     }
+
+    private List<Assertion> assertionList;
 }
---
+++ src/Model/Tree/LeafTask.java
@@ -0,0 +1,10 @@
+package Model.Tree;
+
+import java.util.List;
+
+/**
+ * Created by ladyn-totorosaure on 22/05/17.
+ */
+public class LeafTask extends Task{
+    public List<Operation> operationList;
+}

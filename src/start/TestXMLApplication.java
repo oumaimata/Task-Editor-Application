cf1a91e8cb2c873ba8e80e2012dd77c7c37e4bd3
Camille Soetaert <camille.soetaert@etu.utc.fr>
2017-05-29 15:54:47 +0200

    Récupération et modification d'une application permettant de lire et de colorer le XML

--- src/start/TestXMLApplication.java
+++ src/start/TestXMLApplication.java
@@ -3,8 +3,10 @@ package start;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.fxmisc.richtext.demo.JavaKeywordsAsync;
 
 /**
  * Created by ladyn-totorosaure on 25/05/17.
@@ -12,14 +14,14 @@ import javafx.stage.Stage;
 public class TestXMLApplication extends Application{
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/text_XML_view.fxml"));
-        Pane root = (Pane) fxmlLoader.load();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Controller/XMLEditorContainer.fxml"));
+        Pane root = fxmlLoader.load();
 
         //get reference on the controller
         //ApplicationController controller = fxmlLoader.getController();
 
         Scene scene = new Scene(root, 600, 400);
-        scene.getStylesheets().add("stylesheet.css");
+        scene.getStylesheets().add(JavaKeywordsAsync.class.getResource("xml-highlighting.css").toExternalForm());
         stage.setTitle("Test vue XML");
         stage.setScene(scene);
         stage.show();

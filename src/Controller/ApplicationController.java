ae8faa9dee6083c31ee857089c23a73addd0e0cb
Pierre-Louis <pierre.louis.lacorte@gmail.com>
2017-06-06 18:59:54 +0200

    [update] update application controller to support list view update

--- src/Controller/ApplicationController.java
+++ src/Controller/ApplicationController.java
@@ -1,16 +1,23 @@
 package Controller;
 
+import Model.Tree.Task;
+import Model.Tree.Tasks;
 import com.yworks.yfiles.graph.styles.ShapeNodeShape;
 import com.yworks.yfiles.graph.styles.ShapeNodeStyle;
 import com.yworks.yfiles.layout.tree.TreeLayout;
 import com.yworks.yfiles.view.GraphControl;
 import com.yworks.yfiles.view.Pen;
 import com.yworks.yfiles.view.input.ICommand;
+import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.paint.Color;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,18 +25,23 @@ import java.time.Duration;
  */
 public class ApplicationController {
 
+    // reference sur le controller du graph
     public GraphControl graphControl;
-    private ShapeNodeStyle orangeNodeStyle = createOrangeStyle();
-    private ShapeNodeStyle blueNodeStyle = createBlueStyle();
-
+    // reference sur le controller de la vue
     public ViewController view;
+    // reference sur l'ensemble des taches du modèle
+    public Tasks tasks;
+    // reference sur la tache selectionnée (dans le cas du graph notamment)
+    public Task currentTask;
 
     public ApplicationController(ViewController view) {
         this.view = view;
     }
 
     public void initialize() {
-
+        // ensemble des tâches à null initialement.
+        // Dans le cas d'un chargement il faudra modifier directement cette valeur
+        tasks = new Tasks();
     }
 
     // Méthode principale a utiliser dans le controller.
@@ -38,6 +50,14 @@ public class ApplicationController {
         // ceci est un exemple pour acceder a un element graphique
         Button test = view.getButton_graph_zoomin();
         test.setText("test test");
+
+        // ajout du listener sur le bouton d'ajout de tache qui va déclancher l'ajout d'une tache par défaut
+        view.getButton_graph_ajouter().setOnAction(evt -> ajoutTache());
+
+        // ajout de la liste de toutes les tâches à la listview taches filles
+        // ce n'est pas ce qu'il y aura dedans mais le fonctionnement est ok.
+        // /!\ A adapter /!\
+        view.getListview_edit_taches_filles().setItems(tasks.getTasks());
     }
 
     // Méthode pour réaliser les bindings des actions et des boutons
@@ -88,5 +108,11 @@ public class ApplicationController {
     public void graphFitContent() {
         graphControl.fitContent();
     }
+
+    public void ajoutTache(){
+        System.out.println("lancement de la methode ajout tache du controller");
+        tasks.addDefaultTache();
+    }
+
 
 }

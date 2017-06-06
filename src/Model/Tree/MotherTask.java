35abbb7c083602197228dfb505c5a7fa383912e5
Pierre-Louis <pierre.louis.lacorte@gmail.com>
2017-06-06 18:55:43 +0200

    [add] create constructor for mother tasks and leaf tasks

--- src/Model/Tree/MotherTask.java
+++ src/Model/Tree/MotherTask.java
@@ -1,10 +1,17 @@
 package Model.Tree;
 
 import Model.GlobalParameters;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
 
 /**
  * Created by Camille on 22/05/17.
  */
 public class MotherTask extends Task{
     private GlobalParameters.TypeConstructeur constructor;
+
+    public MotherTask(IntegerProperty idProperty, StringProperty nameProperty, GlobalParameters.TypeConstructeur constructor) {
+        super(idProperty, nameProperty);
+        this.constructor = constructor;
+    }
 }
